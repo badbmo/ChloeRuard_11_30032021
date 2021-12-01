19e3a6168381a9012ca829ee7e9ad3c77663d2c5
Chloé R <clositi@live.fr>
2021-12-01 19:01:50 +0100

    created tag component (basic) added style (basic)

---
+++ src/components/tag.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/tag.css"
+
+export default class Tag extends React.Component {
+	render() {
+		return (
+			<div className="tag__box" aria-label="Tag">
+				<span className="tag__content">coucou</span>
+			</div>
+			
+		);
+	}
+}
