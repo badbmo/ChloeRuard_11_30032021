d13f4e692ab9fe2fd07193e308c5c338ba3ce3e2
Chloé R <clositi@live.fr>
2021-12-03 00:05:42 +0100

    created thumb component (basic) added style (basic)

---
+++ src/components/Thumb.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/thumb.css";
+
+export default class Thumb extends React.Component {
+	render() {
+		return (
+			<article className="thumb">
+              <img src={this.props.cover} alt={`${this.props.title}`} className="thumb__cover"/>
+          <h2 className="thumb__title">{this.props.title}</h2>
+			</article>
+		);
+	}
+}
