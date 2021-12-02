bf18e02e4247b86a9cb67fc4c3871d3a3e60b41b
Chloé R <clositi@live.fr>
2021-12-03 00:24:30 +0100

    created rentalsList component (basic) added style (basic)

---
+++ src/components/RentalsList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../style/rentalsList.css";
+import { Link } from "react-router-dom";
+import Thumb from "./Thumb";
+
+export default class RentalsList extends React.Component {
+	render() {
+		return (
+			<section className="rentalsList">
+				{this.props.data.map((rental) => (
+					<Link key={rental.id} to={`/rental/${rental.id}`}>
+						<Thumb title={rental.title} cover={rental.cover} />
+					</Link>
+				))}
+			</section>
+		);
+	}
+}
