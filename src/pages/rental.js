82c9611dc097c9fd8f378e81cafbf06aa4aa7a9f
Chloé R <clositi@live.fr>
2021-12-04 00:25:50 +0100

    Added Accordion to Rental page

--- src/pages/rental.js
+++ src/pages/rental.js
@@ -2,54 +2,64 @@ import React from "react";
 import "../style/rental.css";
 //import Tag from "../components/tag";
 import Loader from "../components/Loader";
+import Accordion from "../components/accordion";
 
 export default class Rental extends React.Component {
-
 	constructor(props) {
-    super(props);
+		super(props);
 
-    this.state = {
+		this.state = {
 			isLoading: false,
-      data: [],
+			data: [],
 			error: null,
-    };
-  }
+		};
+	}
 
-  componentDidMount() {
+	componentDidMount() {
 		//const {id} = this.props.match.params.id
 
-		this.setState({ isLoading: true })
-    fetch('../data.json')
-      .then(response => response.json())
-      .then(result => this.setState({ data: result.find((location) => 
-					location.id === this.props.match.params.id
-				), isLoading: false }))
-			.catch(error => this.setState({ error, isLoading: false }));
-  }
+		this.setState({ isLoading: true });
+		fetch("../data.json")
+			.then((response) => response.json())
+			.then((result) =>
+				this.setState({
+					data: result.find(
+						(location) =>
+							//location.id === this.props.match.params.id
+							location.id === "c67ab8a7"
+					),
+					isLoading: false,
+				})
+			)
+			.catch((error) => this.setState({ error, isLoading: false }));
+	}
 
 	render() {
-		const {data, isLoading, error } = this.state;
+		const { data, isLoading, error } = this.state;
 		console.log(this.state.data);
-		
-    if (error) {
-      return <p>{error.message}</p>;
-    }
+		const { equipments, description } = this.state.data;
+
+		if (error) {
+			return <p>{error.message}</p>;
+		}
 
 		return (
 			<main className="rental">
-				<h1 className="rental__title">
-					Voici la page locations
-				</h1>
+				<h1 className="rental__title">Voici la page locations</h1>
 				{isLoading ? (
-          <Loader/>
-      ) : (
-				<div>
-				{/* {data.tags.map((tag)=>(
+					<Loader />
+				) : (
+					<div>
+						{/* {data.tags.map((tag)=>(
 					<Tag tag={tag}/>
 				))} */}
-				{data}
-				</div>
-			)}				
+						{data.id}
+						<div className="accordion__container">
+							<Accordion title="Equipements" list={equipments} />
+							<Accordion title="Description" text={description} />
+						</div>
+					</div>
+				)}
 			</main>
 		);
 	}
