8cdfb73e480b97861122e3eab83a4b72629cd626
Chloé R <clositi@live.fr>
2021-12-03 22:36:39 +0100

    fetched data for home page (successfull)

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,50 @@
 import React from "react";
 import "../style/home.css"
-import Tag from "../components/tag";
 import Banner from "../components/banner";
 import Background from "../assets/home_banner.png";
+import Loader from "../components/Loader";
+import RentalsList from "../components/RentalsList";
 
 export default class Home extends React.Component {
+	constructor(props) {
+    super(props);
+
+    this.state = {
+			isLoading: false,
+      data: [],
+			error: null,
+    };
+  }
+
+		/**
+	 * fetch data from json file
+	 */
+  componentDidMount() {
+		this.setState({ isLoading: true })
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => this.setState({ data: result, isLoading: false }))
+			.catch(error => this.setState({ error, isLoading: false }));
+  }
+
 	render() {
+		const { data, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
 		return (
 			<main className="home">
 				<h1 className="home__title">
-					Voici la page accueil
+					Chez vous, partout et ailleurs
 				</h1>
 				<Banner src={Background} alt="paysage de montagnes avec forêt"/>
-				<Tag/>
+
+				{isLoading ? (
+          <Loader/>
+      ) : (
+				<RentalsList data={data}/>)}
 			</main>
 		);
 	}
