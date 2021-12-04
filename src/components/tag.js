163ed1ae2f60034fe21d7d05d4eeec716bed7122
Chloé R <clositi@live.fr>
2021-12-04 01:23:56 +0100

    modified Tag component to list of Tag component

--- src/components/tag.js
+++ src/components/tag.js
@@ -3,11 +3,19 @@ import "../style/tag.css"
 
 export default class Tag extends React.Component {
 	render() {
+		if(this.props.tags){
 		return (
-			<div className="tag__box" aria-label="Tag">
-				<span className="tag__content">coucou</span>
+			<div className="tag__container">
+			{this.props.tags.map((tag) => (
+			<div className="tag__box" aria-label="Tag" key={tag}>
+				<span className="tag__content">{tag}</span>
 			</div>
-			
-		);
+			))}
+			</div>
+		)}else{
+			return(
+			<div className="tag__container"></div>)
+		}
+		;
 	}
 }
