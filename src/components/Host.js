33cb9f2c83683b27c032bb23d097f18446a38435
Chloé R <clositi@live.fr>
2021-12-06 12:23:50 +0100

    created Host component (basic) added style (basic)

---
+++ src/components/Host.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../style/host.css";
+
+export default class Host extends React.Component {
+	render() {
+		if(this.props.host){
+		return (
+			<article className="host">
+				<span className="host__name">{this.props.host.name.split(" ")[0]}<br />{this.props.host.name.split(" ")[1]}</span>
+        <img src={this.props.host.picture} alt={`${this.props.host.name}`} className="host__picture"/>
+			</article>
+		)}else{
+			return(
+				<article classNae="host"></article>
+			)
+		};
+	}
+}
