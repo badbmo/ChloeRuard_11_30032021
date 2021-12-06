9b317059691d4deb42a017da3148bb111b46ac01
Chloé R <clositi@live.fr>
2021-12-06 22:23:56 +0100

    fetched successfully data with id

--- src/pages/rental.js
+++ src/pages/rental.js
@@ -7,6 +7,7 @@ import Host from "../components/Host";
 import Rating from "../components/Rating";
 import Carousel from "../components/Carousel";
 
+
 export default class Rental extends React.Component {
 	constructor(props) {
 		super(props);
@@ -19,22 +20,29 @@ export default class Rental extends React.Component {
 	}
 
 	componentDidMount() {
-		//const {id} = this.props.match.params.id
-
 		this.setState({ isLoading: true });
+		const url = window.location.pathname;
+		this.id = url.split('rental/')[1]
+		this.fetchData();
+		// normaly would use const {id} = this.props.match.params.id BUT react router V6 components rendered via the element prop
+		// (see App.js) don't receive route props (match). Hook and usePrams are mandatory with react V6
+		// To keep Class component here (for the exercice), I used Vanilla JS to get the id in the URL.
+		// It is not recommended.
+	}
+		
+	fetchData(){
 		fetch("../data.json")
-			.then((response) => response.json())
-			.then((result) =>
-				this.setState({
-					data: result.find(
-						(location) =>
-							//location.id === this.props.match.params.id
-							location.id === "c67ab8a7"
-					),
-					isLoading: false,
-				})
-			)
-			.catch((error) => this.setState({ error, isLoading: false }));
+		.then((response) => response.json())
+		.then((result) =>
+			this.setState({
+				data: result.find(
+					(location) =>
+						location.id === this.id
+				),
+				isLoading: false,
+			})
+		)
+		.catch((error) => this.setState({ error, isLoading: false }));
 	}
 
 	render() {
