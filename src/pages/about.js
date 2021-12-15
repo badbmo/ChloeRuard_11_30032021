5e31e6cc01d9aa8e38e015109e468197d36df48d
Chloé R <clositi@live.fr>
2021-12-15 17:10:20 +0100

    correction css collapse

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,6 +29,6 @@ const contentRespect =
 const titleService = "Service";
 const contentService =
 	"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.";
-const titleSecurity = "Security";
+const titleSecurity = "Securité";
 const contentSecurity =
 	"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.";
